c78d83fe3854ed2f058a1b8a54ef6d62ec7dda4d
Поваринцевы <diflust@mail.ru>
2025-01-06 21:43:02 +1000

    Create library for pipeline

---
+++ Groovy_scripts/PipelineUtils.groovy
@@ -0,0 +1,16 @@
+public class PipelineUtils{
+
+    private String lesson;
+    private String msgInfo;
+
+    public String getInfo(String lesson, String msgInfo) {
+        String this.lesson = lesson;
+        String this.msgInfo = msgInfo;
+        if ((lesson != "") && (msgInfo != "")){
+
+        } else {
+
+        }
+    }
+
+}
